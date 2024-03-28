215a460d394c036499f34c7181fa7dba3dc6d267
Xuanwo <Xuanwo@users.noreply.github.com>
2024-03-28 15:58:09 +0000

    deploy: 140b24d2ce1be337844240b511f563c011659837

---
+++ docs/rust/trait.impl/bytes/buf/buf_mut/trait.BufMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"opendal":[["impl BufMut for <a class=\"struct\" href=\"opendal/raw/oio/struct.WritableBuf.html\" title=\"struct opendal::raw::oio::WritableBuf\">WritableBuf</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
