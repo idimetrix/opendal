c91da5e663686862aa1bf888537ca5870b692424
Xuanwo <Xuanwo@users.noreply.github.com>
2024-06-17 14:34:19 +0000

    deploy: dbb9b9ffe6eff9261765c4606bc843853bbb490e

--- docs/dav-server-opendalfs/src-files.js
+++ docs/dav-server-opendalfs/src-files.js
@@ -1,1 +1,1 @@
-var srcIndex=new Map(JSON.parse('[["dav_server_opendalfs",["",[],["dir_entry.rs","file.rs","lib.rs","metadata.rs","opendalfs.rs"]]]]'));createSrcSidebar();
+var srcIndex=new Map(JSON.parse('[["dav_server_opendalfs",["",[],["dir.rs","file.rs","fs.rs","lib.rs","metadata.rs","utils.rs"]]]]'));createSrcSidebar();
